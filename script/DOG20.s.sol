de37b80364694c8b8912ef7ec9d01d53c11ef22d
Ken Van den Enden <ken@enden.be>
2024-02-29 17:50:45 +0100

    Deploy bridged dog.

--- script/DOG20.s.sol
+++ script/DOG20.s.sol
@@ -18,3 +18,29 @@ contract Deploy is Script {
         vm.stopBroadcast();
     }
 }
+
+interface IOptimismMintableERC20Factory {
+    function createOptimismMintableERC20(address _remoteToken, string memory _name, string memory _symbol)
+        external
+        returns (address);
+}
+
+contract DeployL2 is Script {
+    function setUp() public {}
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        address dog20 = vm.envAddress("DOG20_ADDRESS");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        IOptimismMintableERC20Factory factory =
+            IOptimismMintableERC20Factory(0x4200000000000000000000000000000000000012);
+
+        address dog20OP = factory.createOptimismMintableERC20(dog20, "The Doge NFT", "DOG");
+        console.log("Bridged DOG20 deployed to address: ", address(dog20OP));
+
+        vm.stopBroadcast();
+    }
+}
