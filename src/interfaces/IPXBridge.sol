4ed5fc9eb98f50baec47424f58c9bbb103a70fab
Ken Van den Enden <ken@enden.be>
2024-07-13 15:48:55 +0200

    Bridge tokens in batch.

--- src/interfaces/IPXBridge.sol
+++ src/interfaces/IPXBridge.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.13;
 interface IPXBridge {
     event Bridge(uint256 indexed tokenId);
 
-    function bridge(uint256 tokenId) external;
+    function bridge(uint256[] memory tokenIds) external;
 }
